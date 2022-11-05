020de91fbb28945f0d49c9d30de815e4d0c6a6ce
Danica Gazdić <danicagazdic@gmail.com>
2022-11-05 14:18:21 +0100

    Added listeners to log in and register buttons

--- app/src/main/java/com/example/uberapp_tim/activities/UserLoginActivity.java
+++ app/src/main/java/com/example/uberapp_tim/activities/UserLoginActivity.java
@@ -1,7 +1,10 @@
 package com.example.uberapp_tim.activities;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 import com.example.uberapp_tim.R;
@@ -11,7 +14,24 @@ public class UserLoginActivity extends Activity {
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.user_login_activity);
-        Toast.makeText(this, "UserLogin", Toast.LENGTH_SHORT).show();
+//        Toast.makeText(this, "UserLogin", Toast.LENGTH_SHORT).show();
+
+
+        Button logInButton = findViewById(R.id.button3);
+        logInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserLoginActivity.this, PassengerMainActivity.class));
+            }
+        });
+
+        Button registerButton = findViewById(R.id.button4);
+        registerButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(UserLoginActivity.this, PassengerRegisterActivity.class));
+            }
+        });
     }
 
     @Override
