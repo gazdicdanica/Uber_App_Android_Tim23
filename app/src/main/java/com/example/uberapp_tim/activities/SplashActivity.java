3c7087b38283a5def0024abcc7a6edd849937f26
Danica Gazdić <danicagazdic@gmail.com>
2022-11-05 12:51:59 +0100

    Initial confiuration of project

---
+++ app/src/main/java/com/example/uberapp_tim/activities/SplashActivity.java
@@ -0,0 +1,56 @@
+package com.example.uberapp_tim.activities;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
+public class SplashActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        Toast.makeText(this, "Splash", Toast.LENGTH_SHORT).show();
+        new Timer().schedule(new TimerTask() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashActivity.this, UserLoginActivity.class));
+                finish();
+            }
+        }, 5000);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+
+}
---
+++ app/src/main/java/com/example/uberapp_tim/activities/UserLoginActivity.java
@@ -0,0 +1,43 @@
+package com.example.uberapp_tim.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class UserLoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        Toast.makeText(this, "UserLogin", Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
