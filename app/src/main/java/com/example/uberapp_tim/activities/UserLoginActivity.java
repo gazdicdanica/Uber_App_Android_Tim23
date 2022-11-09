0729cfc106282b7ac77cfbce323bbd6da0fe2e73
Danica Gazdić <danicagazdic@gmail.com>
2022-11-09 18:18:53 +0100

    Finished login page and added customized launcher icons

--- app/src/main/java/com/example/uberapp_tim/activities/UserLoginActivity.java
+++ app/src/main/java/com/example/uberapp_tim/activities/UserLoginActivity.java
@@ -7,16 +7,18 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.example.uberapp_tim.R;
 
-public class UserLoginActivity extends Activity {
+public class UserLoginActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.user_login_activity);
 //        Toast.makeText(this, "UserLogin", Toast.LENGTH_SHORT).show();
 
-
+        getSupportActionBar().hide();
         Button logInButton = findViewById(R.id.button3);
         logInButton.setOnClickListener(new View.OnClickListener() {
             @Override
