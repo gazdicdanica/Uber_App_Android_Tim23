d85dec8031682bf102d3d39aa0c41b523fb9ba07
unknown <isomidobradovic@gmail.com>
2022-11-12 00:31:28 +0100

    Finishing DriverMain and its derivatives

--- app/src/main/java/com/example/uberapp_tim/activities/DriverPapersActivity.java
+++ app/src/main/java/com/example/uberapp_tim/activities/DriverPapersActivity.java
@@ -2,14 +2,26 @@ package com.example.uberapp_tim.activities;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.uberapp_tim.R;
 
-public class DriverPapersActivity extends Activity {
+public class DriverPapersActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstance){
         super.onCreate(savedInstance);
         setContentView(R.layout.driver_papers_activity);
+
+        Button confirm = findViewById(R.id.confirmDriverIds);
+        confirm.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 
     @Override
