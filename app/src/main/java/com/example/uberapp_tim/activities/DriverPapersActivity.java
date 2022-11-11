ef157117c7e71546adaf9e624704f45416999e17
unknown <isomidobradovic@gmail.com>
2022-11-12 00:05:34 +0100

    Working on driver account activity and changing views to match responsive design

---
+++ app/src/main/java/com/example/uberapp_tim/activities/DriverPapersActivity.java
@@ -0,0 +1,44 @@
+package com.example.uberapp_tim.activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.uberapp_tim.R;
+
+public class DriverPapersActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstance){
+        super.onCreate(savedInstance);
+        setContentView(R.layout.driver_papers_activity);
+    }
+
+    @Override
+    protected void onStart(){
+        super.onStart();
+    }
+
+    @Override
+    protected void onRestart(){
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy(){
+        super.onDestroy();
+    }
+}
